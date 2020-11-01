b1488b0bedfd1e823bac05bde97b2a65727cf800
Kazu <nkz9528w.econ@gmail.com>
2020-11-01 14:52:22 +0900

    Use react

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,10 @@
-console.log("aaa");
+import React from "react";
+import ReactDOM from "react-dom";
+import { App } from "./components/App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
